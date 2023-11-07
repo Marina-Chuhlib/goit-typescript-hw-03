1c9a025a4df91f6fb0f9dddbb9fd0032b26a8738
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-11-07 14:05:20 +0300

    Done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  protected door = false;
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (!this.door) {
+      throw new Error("Door is close");
+    }
+
+    this.tenants.push(person);
+
+    console.log("Person inside");
+  }
+
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() !== this.key.getSignature()) {
+      throw new Error("Key to another door");
+    }
+
+    return (this.door = true);
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
